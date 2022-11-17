9434838ef1eb432bb58321618c084d834f7ababa
PrzeChoj <premysl.choj@gmail.com>
2022-11-17 23:16:09 +0100

    Zmiany po zajeciach

--- MonteCarlo/P1Z60_PCH_MonteCarlo.m
+++ MonteCarlo/P1Z60_PCH_MonteCarlo.m
@@ -1,9 +1,9 @@
-function [integral] = P1Z60_PCH_MonteCarlo(f, randPoints, type, betaParams, dividePoints)
+function [integral] = P1Z60_PCH_MonteCarlo(f, randPoints, type, params)
 % Projekt 1, zadanie 60
 % Przemyslaw Chojecki, 298814
 % 
-% Obliczanie całki podwójnej metodami Monte Carlo
-% na kwadracie [0,1]x[0,1].
+% Obliczanie całki podwójnej metodami
+% Monte Carlo na kwadracie [0,1]x[0,1].
 % 
 % Wejscie:
 %   f            - uchwyt do funkcji 2 zmiennych, ktora bedzie calkowana
@@ -12,13 +12,13 @@ function [integral] = P1Z60_PCH_MonteCarlo(f, randPoints, type, betaParams, divi
 %                  (domyslenie "basic"); jedno z: "basic",
 %                  "quasiMC", "reimann", "imporftanceSampling",
 %                  "latinHypercube", "divideAndConquer"
-%   betaParams   - sprawdzany jedynie dla type = "imporftanceSampling";
-%                  wektor długości 4 reprezentujący parametry rozkładu
-%                  Beta kolejno (alpha_1, beta_1, alpha_2, beta_2)
-%                  (domyslnie (1,1,1,1))
-%   dividePoints - sprawdzany jedynie dla type = "divideAndConquer";
-%                  liczba punktów użytych do estymacji wariancji
-%                  w każdym kroku przed podziałem (domyslenie 10)
+%   params       - dla type = "imporftanceSampling":
+%                     wektor długości 4 reprezentujący parametry rozkładu
+%                     Beta kolejno (alpha_1, beta_1, alpha_2, beta_2)
+%                     (domyslnie (1,1,1,1))
+%                  dla type = "divideAndConquer":
+%                     liczba punktów użytych do estymacji wariancji
+%                     w każdym kroku przed podziałem (domyslenie 10)
 % 
 % Wyjscie:
 %   integral - estymowana wartosc calki
