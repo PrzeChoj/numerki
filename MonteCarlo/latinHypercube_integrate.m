65076f3ec254ada099336030adbc50a1afdc8741
PrzeChoj <premysl.choj@gmail.com>
2022-12-06 00:07:00 +0100

    latinHypercube implemented

--- MonteCarlo/latinHypercube_integrate.m
+++ MonteCarlo/latinHypercube_integrate.m
@@ -1,7 +1,17 @@
-function [integral] = latinHypercube_integrate(f, randPoints)
-% Based on page 360 of Handbook of MC methods by
+function [integral] = latinHypercube_integrate(f, randPoints, K)
+% Based on page 361 of Handbook of MC methods by
 % Dirk P. Kroese, Thomas Taimre, and Zdravko I. Botev
 
+N = floor(randPoints/K);
 integral = 0;
+
+for n=1:N
+    us = rand(2,K);
+    [~,p] = sort(rand(K,2));
+    V = (p' - us)/K; % bez + 1
+    integral = integral + sum(f(V(1,:), V(2,:))) / K;
+end
+
+integral = integral/N;
 
 end
