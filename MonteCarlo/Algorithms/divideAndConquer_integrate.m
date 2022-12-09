2b97657cd7661518f9dfa6bd5aeb5593b3db1bc8
PrzeChoj <premysl.choj@gmail.com>
2022-12-09 01:27:58 +0100

    sprzatanko

--- MonteCarlo/Algorithms/divideAndConquer_integrate.m
+++ MonteCarlo/Algorithms/divideAndConquer_integrate.m
@@ -3,4 +3,6 @@ function [integral, points] = divideAndConquer_integrate(f, randPoints, n_var_es
 [integral, points] = divideAndConquer_integrate_recurrent(f, ...
     randPoints, n_var_est, 0, 1, 0, 1);
 
+% TODO(delete zeros in points)
+
 end % function
