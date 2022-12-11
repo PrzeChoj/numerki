c777886b18e8b36fc5546d4c9027335a602e922d
PrzeChoj <premysl.choj@gmail.com>
2022-12-11 16:42:15 +0100

    divideAndConquer usun zera

--- MonteCarlo/Algorithms/divideAndConquer_integrate.m
+++ MonteCarlo/Algorithms/divideAndConquer_integrate.m
@@ -3,6 +3,11 @@ function [integral, points] = divideAndConquer_integrate(f, randPoints, n_var_es
 [integral, points] = divideAndConquer_integrate_recurrent(f, ...
     randPoints, n_var_est, 0, 1, 0, 1);
 
-% TODO(delete zeros in points)
+% points ma czasem 0; Trzeba się tych zer pozbyć
+non_zers = (points(1,:) ~= 0);
+points_out = zeros(2,sum(non_zers));
+points_out(1,:) = points(1,non_zers);
+points_out(2,:) = points(2,non_zers);
+points = points_out;
 
 end % function
